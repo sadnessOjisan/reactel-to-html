b10130378ea40daa5ddfdefd3e642fd733f22156
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-22 22:31:47 +0900

    api

--- api/convert.ts
+++ api/convert.ts
@@ -5,5 +5,5 @@ export default (req: NowRequest, res: NowResponse) => {
   const body = req.body;
   const { html } = body;
   const convertedHtml = lib.default(html);
-  res.json(convertedHtml);
+  res.status(200).json(convertedHtml);
 };
