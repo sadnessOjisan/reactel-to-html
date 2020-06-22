29f67d958a3dbea45e6ffb36d7f0d743d979311e
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-22 20:48:04 +0900

    update server

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 convert html to jsx and convert the jsx to html
 
+server
+
+```
+$ curl -X POST -H "Content-Type: application/json" -d '{"Name":"sensuikan1973", "Age":"100"}' localhost:8080/api/v1/users
+```
+
 ## todo
 
 - [ ] サーバー
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const lib = require("../dist/index");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+  const body = req.body;
+  const { html } = body;
+  const convertedHtml = lib.default(html);
+  res.json(convertedHtml);
+});
+
+app.listen(3000);
